7e9eaadf4dda78773c81f5c046f0a847d124b3fe
practicaltkdonline <practical.tkd.online@gmail.com>
2026-01-05 22:35:24 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'attendance-v1';
+const urlsToCache = [
+  './',
+  './manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
